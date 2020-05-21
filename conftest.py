d9f0464475c4855e1cb56b093f9f9db6572bfc4a
Parin <parin@Parins-MBP.home>
2020-05-21 13:10:18 -0400

    adding pipeline versioning

--- conftest.py
+++ conftest.py
@@ -21,6 +21,7 @@ def pipeline(sch, request):
     pipeline_id = request.config.getoption('pipeline_id')
     pipeline_ = sch.pipelines.get(pipeline_id=pipeline_id)
     environment = request.config.getoption('environment')
+    pipeline_version = request.config.getoption('pipeline_version')
 
     yield pipeline_
 
@@ -28,15 +29,24 @@ def pipeline(sch, request):
     if jobs_to_delete:
         logger.debug('Deleting test jobs: %s ...', ', '.join(str(job) for job in jobs_to_delete))
         sch.delete_job(*jobs_to_delete)
+
     if not request.session.testsfailed:
         if request.config.getoption('upgrade_jobs'):
             pipeline_ = sch.pipelines.get(pipeline_id=pipeline_id)
             jobs_to_upgrade = [job for job in sch.jobs.get_all(pipeline_id=pipeline_id)
                                if (job.pipeline_commit_label != f'v{pipeline_.version}'
                                    and job.job_name.startswith(environment))]
-            if jobs_to_upgrade:
+            if jobs_to_upgrade and pipeline_version == 'LATEST':
                 logger.info('Upgrading jobs: %s ...', ', '.join(str(job) for job in jobs_to_upgrade))
                 sch.upgrade_job(*jobs_to_upgrade)
+            elif jobs_to_upgrade and pipeline_version != 'LATEST':
+                pipeline_commit = pipeline_.commits.get(version=pipeline_version)
+
+                logger.info('Upgrading jobs: %s ...', ', '.join(str(job) for job in jobs_to_upgrade))
+                for job in jobs_to_upgrade:
+                    job.commit = pipeline_commit
+                    sch.update_job(job)
+
             else:
                 logger.warning('No jobs need to be upgraded')
 
